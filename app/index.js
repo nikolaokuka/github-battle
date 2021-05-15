4a82cae9f5eb31adbb59a0fada5b49514a6d80fb
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-15 14:36:54 +0200

    init commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App () {
+  return (
+    <h1>Github Battle</h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
