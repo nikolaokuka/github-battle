e09f0583693040941afab3c6bfb0df3dcef81725
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-18 06:41:04 +0200

    calculate winner

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { battle } from '../utils/api'
 
 export default class Results extends Component {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  }
+
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props
+
+    battle([playerOne, playerTwo])
+      .then(([winner, loser]) => {
+        this.setState({
+          winner,
+          loser,
+          error: null,
+          loading: false
+        })
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
