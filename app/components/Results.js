e16bb867ca4f747a2de371272c7fcef57be3b304
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-18 08:46:32 +0200

    add CardList component

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react'
 import { battle } from '../utils/api'
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
 import Card from './Card'
+import PropTypes from 'prop-types'
+
+function CardList({ profile }) {
+  return (
+    <ul className='card-list'>
+      <li>
+        <FaUser color='rgb(239, 115, 115)' size={22} />
+        {profile.name}
+      </li>
+      {profile.location &&
+        <li>
+          <FaCompass color='rgb(144, 115, 255)' size={22} />
+          {profile.location}
+        </li>
+      }
+      {profile.company &&
+        <li>
+          <FaBriefcase color='#795548' size={22} />
+          {profile.company}
+        </li>
+      }
+      <li>
+        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        {profile.followers.toLocaleString()} followers
+      </li>
+      <li>
+        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        {profile.following.toLocaleString()} following
+      </li>
+    </ul>
+  )
+}
+
+CardList.propTypes = {
+  profile: PropTypes.object.isRequired,
+}
 
 export default class Results extends Component {
   state = {
@@ -51,32 +87,7 @@ export default class Results extends Component {
           score={`Score: ${winner.score.toLocaleString()}`}
           url={winner.profile.html_url}
         >
-          <ul className='card-list'>
-            <li>
-              <FaUser color='rgb(239, 115, 115)' size={22} />
-              {winner.profile.name}
-            </li>
-            {winner.profile.location &&
-              <li>
-                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                {winner.profile.location}
-              </li>
-            }
-            {winner.profile.company &&
-              <li>
-                <FaBriefcase color='#795548' size={22} />
-                {winner.profile.company}
-              </li>
-            }
-            <li>
-              <FaUsers color='rgb(129, 195, 245)' size={22} />
-              {winner.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-              {winner.profile.following.toLocaleString()} following
-            </li>
-          </ul>
+          <CardList profile={winner.profile} />
         </Card>
 
         <Card
@@ -86,32 +97,7 @@ export default class Results extends Component {
           score={`Score: ${loser.score.toLocaleString()}`}
           url={loser.profile.html_url}
         >
-          <ul className='card-list'>
-            <li>
-              <FaUser color='rgb(239, 115, 115)' size={22} />
-              {loser.profile.name}
-            </li>
-            {loser.profile.location &&
-              <li>
-                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                {loser.profile.location}
-              </li>
-            }
-            {loser.profile.company &&
-              <li>
-                <FaBriefcase color='#795548' size={22} />
-                {loser.profile.company}
-              </li>
-            }
-            <li>
-              <FaUsers color='rgb(129, 195, 245)' size={22} />
-              {loser.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-              {loser.profile.following.toLocaleString()} following
-            </li>
-          </ul>
+          <CardList profile={loser.profile} />
         </Card>
       </div>
     )
