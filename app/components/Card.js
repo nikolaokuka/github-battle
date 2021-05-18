0a7e027c6d749128c19dbd63f6e7f2efeaf9c927
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-18 08:28:16 +0200

    reusable Card component

---
+++ app/components/Card.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Card({ header, avatar, username, score, url, children }) {
+  return (
+    <div className='card bg-light'>
+      <h4 className='header-lg center-text'>
+        {header}
+      </h4>
+      <img
+        className='avatar'
+        src={avatar}
+        alt={`Avatar for ${username}`}
+      />
+      {score &&
+        <h4 className='center-text'>
+          Score: {score}
+        </h4>
+      }
+      <h2 className='center-text'>
+        <a className='link' href={url}>{username}</a>
+      </h2>
+      {children}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  header: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+}
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { battle } from '../utils/api'
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+import Card from './Card'
 
 export default class Results extends Component {
   state = {
@@ -43,21 +44,13 @@ export default class Results extends Component {
 
     return (
       <div className='grid space-around container-sm'>
-        <div className='card bg-light'>
-          <h4 className='header-lg center-text'>
-            {winner.score === loser.score ? 'Tie' : 'Winner'}
-          </h4>
-          <img
-            className='avatar'
-            src={winner.profile.avatar_url}
-            alt={`Avatar for ${winner.profile.login}`}
-          />
-          <h4 className='center-text'>
-            Score: {winner.score.toLocaleString()}
-          </h4>
-          <h2 className='center-text'>
-            <a className='link' href={winner.profile.html_url}>{winner.profile.login}</a>
-          </h2>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          avatar={winner.profile.avatar_url}
+          username={winner.profile.login}
+          score={`Score: ${winner.score.toLocaleString()}`}
+          url={winner.profile.html_url}
+        >
           <ul className='card-list'>
             <li>
               <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -84,23 +77,15 @@ export default class Results extends Component {
               {winner.profile.following.toLocaleString()} following
             </li>
           </ul>
-        </div>
+        </Card>
 
-        <div className='card bg-light'>
-          <h4 className='header-lg center-text'>
-            {winner.score === loser.score ? 'Tie' : 'Loser'}
-          </h4>
-          <img
-            className='avatar'
-            src={loser.profile.avatar_url}
-            alt={`Avatar for ${loser.profile.login}`}
-          />
-          <h4 className='center-text'>
-            Score: {loser.score.toLocaleString()}
-          </h4>
-          <h2 className='center-text'>
-            <a className='link' href={loser.profile.html_url}>{loser.profile.login}</a>
-          </h2>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          avatar={loser.profile.avatar_url}
+          username={loser.profile.login}
+          score={`Score: ${loser.score.toLocaleString()}`}
+          url={loser.profile.html_url}
+        >
           <ul className='card-list'>
             <li>
               <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -127,7 +112,7 @@ export default class Results extends Component {
               {loser.profile.following.toLocaleString()} following
             </li>
           </ul>
-        </div>
+        </Card>
       </div>
     )
   }
