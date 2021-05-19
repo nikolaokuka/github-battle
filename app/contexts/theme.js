f9d6a78100b20f2af79fa513232e02de84b09b24
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-19 23:42:27 +0200

    add ThemeProvider

---
+++ app/contexts/theme.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react'
+
+const { Provider, Consumer } = createContext()
+
+export const ThemeProvider = Provider
+export const ThemeConsumer = Consumer
--- app/index.js
+++ app/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import './index.css'
 
-function App() {
-  return (
-    <div className='container'>
-      <Battle />
-    </div>
-  )
+class App extends Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => this.setState(({ theme }) => ({
+      theme = 'light' ? 'dark' : 'light'
+    }))
+  }
+
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 ReactDOM.render(
