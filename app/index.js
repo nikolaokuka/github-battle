b675e16b622251bdd036a70a1b7bfd87ee7ee89f
Nikola Okuka <nikolaokuka@gmail.com>
2021-05-20 14:59:18 +0200

    add dynamic imports

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Results from './components/Results'
+import Loading from './components/Loading'
 import Nav from './components/Nav'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends Component {
   state = {
@@ -24,12 +26,14 @@ class App extends Component {
             <div className='container'>
               <Nav />
 
-              <Switch>
-                <Route exact path='/' component={Popular} />
-                <Route exact path='/battle' component={Battle} />
-                <Route path='/battle/results' component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path='/' component={Popular} />
+                  <Route exact path='/battle' component={Battle} />
+                  <Route path='/battle/results' component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
